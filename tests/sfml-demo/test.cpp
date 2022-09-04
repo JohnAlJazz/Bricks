19196c21795056b7c1e7db5779656de29d51502b
JohnAlJazz <111443732+JohnAlJazz@users.noreply.github.com>
2022-09-04 15:09:41 +0300

    circle and rectangle

---
+++ tests/sfml-demo/test.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition(400, 400);
+    sf::RectangleShape rectangle(sf::Vector2f(350.f, 120.f));
+    rectangle.setPosition(400, 400);
+    
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.draw(rectangle);
+        window.display();
+    }
+
+    return 0;
+}
