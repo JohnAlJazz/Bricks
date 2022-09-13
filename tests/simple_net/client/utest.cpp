dc7879d412537a1aab5cc1476c2c1bd8741929a1
JohnAlJazz <111443732+JohnAlJazz@users.noreply.github.com>
2022-09-13 22:18:27 +0300

    vector of 1000 doubles test added

--- tests/simple_net/client/utest.cpp
+++ tests/simple_net/client/utest.cpp
@@ -5,22 +5,41 @@
 #include <assert.h>
 #include <iostream>
 #include <vector>
+#include <numeric>
+
+std::vector<double> RandDoubleInitializer() {
+    std::vector<double> doubleVector;
+    for(size_t i = 0; i < 1000; ++i) {
+        double d = rand() % 10;
+        doubleVector.push_back(d);
+    }
+    return doubleVector;
+}
+
 
 BEGIN_TEST(client) 
     using namespace net;
     std::vector<int> vec{1,2,3,4,5,6,7,8,9,10};
     Client client("127.0.0.1", 8080);    
-    client.Send(vec);
-    // client. Receive();    
-    
+    client.Send(vec);    
+    ASSERT_PASS();
+END_TEST
+
+BEGIN_TEST(random_double_test) 
+    using namespace net;
+    std::vector<double> vector = RandDoubleInitializer();
+    double sum = std::accumulate(vector.begin(), vector.end(), 0);
+    Client client("127.0.0.1", 8080);    
+    client.Send(vector);
+    std::cout << "sum of vector sent from TCP client: " << sum << '\n';      
     ASSERT_PASS();
 END_TEST
 
 
 
 
-
 BEGIN_SUITE(Its what you learn after you know it all that counts)
-	TEST(client)   	
+	TEST(client) 
+    TEST(random_double_test)  	
     		
 END_SUITE
--- tests/simple_net/server/utest.cpp
+++ tests/simple_net/server/utest.cpp
@@ -7,26 +7,38 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <numeric>
 
 
 BEGIN_TEST(server) 
     using namespace net;
     Server server(8080);
-    std::vector<int> v;    
+    std::vector<int> vec;    
     server.Listen();    
     std::unique_ptr<Client> client = server.Accept(); 
-    v = client->Receive<std::vector<int>>(); 
-    for(auto e : v) {
+    vec = client->Receive<std::vector<int>>(); 
+    for(auto e : vec) {
         std::cout << e << '\n';
     }  
     ASSERT_PASS();
 END_TEST
 
 
+BEGIN_TEST(large_vec_test) 
+    using namespace net;
+    Server server(8080);
+    std::vector<double> v;    
+    server.Listen();    
+    std::unique_ptr<Client> client = server.Accept(); 
+    v = client->Receive<std::vector<double>>(); 
+    double sum = std::accumulate(v.begin(), v.end(), 0);
+    std::cout << "sum of vector accepted from TCP server: " << sum << '\n';  
+    ASSERT_PASS();
+END_TEST
+
 
 
 BEGIN_SUITE(Its what you learn after you know it all that counts)
 	TEST(server)
-    	
-    		
+    TEST(large_vec_test)    		
 END_SUITE
